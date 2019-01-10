cc179ec8f76c27bfaecb45e9427233250cbed605
Peter Teuben <teuben@gmail.com>
2019-01-09 21:29:15 -0500

    cleanup documentation

--- hi-observe.py
+++ hi-observe.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python
 #
-#  HI-observe:  Load a RA-DEC-VEL  FITS cube ,
+#  HI-observe:  Load an RA-DEC-VEL FITS cube ,
 #               extract the spectrum at a given pixel/ra+dec/glon+glat
 #               plot the spectrum
 #
 #  5-jan-2019   PJT hacking, based on a trimmed cubespectrum3.py, for the GradMap weekend at the 40ft
 #  8-jan-2018   PJT finished documenting, added the coordinate transformation functions
-#                   make it work on BL.fits 
+#                   make it work on BL.fits and LAB.fits, add Makefile for easy bootstrapping
 #
 #  Caveats:
 #  - this code still ignores the equinox (B1950, J2000, current)
@@ -21,7 +21,7 @@ from astropy.io import fits
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 
-#  define some convenience functions
+#  define some convenience functions to transform coordinates between RA,DEC <---> GLON,GLAT
 
 def radec_glonlat(rah,ram,ras,dec):
     """
@@ -53,13 +53,15 @@ if True:
     print("test1: radec_glonlat(20,58,15,42) should produce 83.886 -2.67928:   %g %g" % (glon,glat))
     print("test2: glonlat_radec(glon,glat)   should produce 315 42:            %g %g" % (ra,dec))
 
-#   some constants we might need
+#   some constants we might need (but actually not in the current code)
 
 c        = 299792.458         # speed of light in km/s
 restfreq = 1420.405751786     # HI restfreq in MHz
 
 
-#   parse our simple command line interface
+#   parse our simple positional command line interface,
+#   and give some help if you didn't specify it correctly
+
 na = len(sys.argv) - 1
 cmdline = str(sys.argv[1:])
 if na == 3:
@@ -89,8 +91,9 @@ elif na == 5:
     print("GLON/GLAT: %g %g" % (glon,glat))
     pos=[glon,glat]
 else:
-    print("Usage: %s fitsfile glon glat" % sys.argv[0])
-    print("       %s fitsfile -xpos -ypos" % sys.argv[0])
+    # else give some reminders
+    print("Usage: %s fitsfile glon glat"       % sys.argv[0])
+    print("       %s fitsfile -xpos -ypos"     % sys.argv[0])
     print("       %s fitsfile rah ram ras dec" % sys.argv[0])
     sys.exit(1)
 
@@ -98,11 +101,11 @@ else:
 # open the fits file (a FITS file has one or more Header-Data-Unit's, HDU's)
 hdu = fits.open(fitsfile)
 
-# get a reference to the primary header and data. data better be 3-dim numpy array now
+# get a reference to the primary header and data.
 h = hdu[0].header
 d = hdu[0].data.squeeze()
 print("Shape of cube: :",d.shape)
-if len(d.shape) != 3:
+if len(d.shape) != 3:                 # The data better be 3-dim numpy array now
     print("Your cube is not 3D")
     sys.exit(1)
 
@@ -123,8 +126,8 @@ cdelt3 = h['CDELT3']
 crval3 = h['CRVAL3']
 crpix3 = h['CRPIX3']
 
-
-# pos[] is either a pixel or GLON/GLAT coordinate
+# pos[] is now either a pixel or GLON/GLAT coordinate
+# depending on the "have_pixel" variable
 xpos = pos[0]
 ypos = pos[1]
 
@@ -137,9 +140,9 @@ if have_pixel:
     (ra,dec) = glonlat_radec(glon,glat)
     print("RA/DEC: %g %g" % (ra,dec))
 else:
-    # now need to convert the xpos,ypos (in WCS) to pixel
-    # xpos_wcs = (xpos_pix - crpix + 1) * cdelt + crval
-    # xpos_pix = (xpos_wcs - crval)/cdelt + crpix - 1
+    # need to convert the xpos,ypos (in WCS) to pixel
+    #    xpos_wcs = (xpos_pix - crpix + 1) * cdelt + crval
+    #    xpos_pix = (xpos_wcs - crval)/cdelt + crpix - 1
     xposl = xpos
     yposb = ypos
     xpos = (xposl - crval1)/cdelt1 + crpix1 - 1 
@@ -149,7 +152,8 @@ else:
     ypos = int(ypos)
     
 
-#   finally we get the spectrum using a numpy slice operation, the Y coordinate in the plot
+#   now we grab get the spectrum using a numpy slice operation
+#   this will be the Y (intensity) coordinate in the plot
 flux     = d[:,ypos,xpos]
 
 #   some helper arrays for the X (velocity) coordinate in the plot
@@ -166,13 +170,12 @@ plt.figure()
 plt.plot(channelv,flux,'o-',markersize=2,label='HI-spectrum')
 plt.plot(channelv,zero,                  label='baseline')
 plt.xlabel("Doppler Velocity (km/s)")
-plt.ylabel("Brightness (K-KM/S")
+plt.ylabel("Brightness")
 plt.title("%s  @ %g %g" % (cmdline,xpos,ypos))
 
 plt.legend()
 plt.savefig('hi-observe.png')
 plt.show()
-    
 
 #   some statistics
 
